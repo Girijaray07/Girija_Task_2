67d1af766f0afd7b85dcaf090879a697239a027a
Girija Ray <91793906+Girijaray07@users.noreply.github.com>
2025-06-25 18:23:49 +0530

    Initial Commit

---
+++ todo.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    print("          ***** Welcome to Python To-Do-List App *****          ")
+
+    print("Select from the Below Option: ")
+    print("1. Add a Work.")
+    print("2. Remove a Work.")
+    print("3. Update a Work.")
